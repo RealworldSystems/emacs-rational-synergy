049170a477b6ac2320c28a1deb88ce1c5f58a57c
unknown <bwillem2@LT-51172.enexis.local>
2015-07-01 18:20:22 +0200

    Started the history refactoring

--- vc-rational-synergy-history.el
+++ vc-rational-synergy-history.el
@@ -43,6 +43,61 @@
 ;;;; CLI.
 
 
+(defun vc-rational-synergy--command-history-file (file-name)
+  "Attempts to gather the history of a file, for basic information
+This is a wrapper around `vc-rational-synergy-command-w/format-to-list'"
+  (condition-case err
+      (vc-rational-synergy-command-w/format-to-list
+       `("history" ,file-name)
+       'object-name 'name 'version 'type 'instance 'task 'owner)
+    (error nil)))
+
+(defun vc-rational-synergy-command-grab-comments (file-name)
+  "Attempts to gather the history of a file, for basic information
+This is a wrapper around `vc-rational-synergy-command'"
+  (condition-case err
+      (let ((raw-comments (vc-rational-synergy-command
+			   `("nch" "-f" ">>>%objectname\t%comment")))
+	    result current-object-name current-line pivot)
+	;; For each line, detect if >>> is present, if so, this is a new
+	;; comment. If not, it is an old comment, and the line needs
+	;; to be appended without the strip with the first tab.
+	;; After >>> immediately the object name follows, separated by a
+	;; tab character.
+	(dolist (raw raw-comments)
+	  (if (and (> (length raw) 3)
+		   (string= ">>>" (substring raw 0 3)))
+	      (progn
+		;; Push current-line and current-object-name into result
+		(setq result (cons `(,current-object-name . ,current-line) result))
+		;; Extract object name
+		(let* ((less-raw (substring raw 4)))
+		  (setq pivot (string-match "\\t" less-raw))
+		  (setq current-object-name (substring less-raw 0 pivot))
+		  (setq current-line (substring less-raw (+ 1 pivot)))))
+	    (setq current-line (concat current-line "\n" (substring raw (+ 5 pivot))))))
+	;; push last line (if available)
+	(when current-line
+	  (setq result (cons `(,current-object-name . ,current-line) result)))
+	result)
+    (error nil)))
+
+(defun vc-rational-synergy--command-get-successors (objectname)
+  "Using the OBJECT name find the successors"
+  (condition-case err
+      (vc-rational-synergy-command-w/format-to-list
+       `("query -u" ,(format "has_predecessor('%s')" objectname))
+       'objectname)
+    (error nil)))
+
+(defun vc-rational-synergy--command-get-predecessors (objectname)
+  "Using the OBJECT name find the predecessors"
+  (condition-case err
+      (vc-rational-synergy-command-w/format-to-list
+       `("query -u" ,(format "has_successors('%s')" objectname))
+       'objectname)
+    (error nil)))
+  
 
 
 
